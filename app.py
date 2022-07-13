6031ed6bb867771ef664f3bd582f435c35fcbbf2
Felipe Henriquez Ricart <fhenriquez2017@inf.udec.cl>
2022-07-12 23:28:16 -0400

    Update app.py

--- app.py
+++ app.py
@@ -54,5 +54,6 @@ if __name__ == "__main__":
     app.run(debug=True)
 '''
 
-if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+if __name__ == "__main__":
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=8080)
